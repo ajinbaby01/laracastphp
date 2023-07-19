63253f553c68a6392cdde497ef40f134731e1a19
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-19 11:24:37 +0530

    PHP Database Object

--- functions.php
+++ functions.php
@@ -3,4 +3,8 @@
 function isUrl($value){
     return parse_url($_SERVER['REQUEST_URI'])['path'] == $value;
 }
+
+function dd($array){
+    echo '<pre>'; var_dump($array); echo '</pre>';
+}
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
-require 'function.php';
-require 'router.php';
+require 'functions.php';
+//require 'router.php';
+
+$dsn = 'mysql:host=localhost;port=3308;dbname=laracastdb;user=ajin;password=123456;charset=utf8mb4';
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
+
 ?>
