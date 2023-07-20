be9fbad54711b45835a57ad5b26ea55cbdfd665e
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-20 16:26:28 +0530

    Don't insert if body is empty

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -9,12 +9,20 @@ $config = require('config.php');
 $db = new Database($config['database'], 'ajin', '123456');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $query = "insert into notes (body, user_id) values(:body, :user_id)";
 
-    $db->query($query, [
-        'body' => filter_var($_POST['body'], FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-        'user_id' => 1
-    ]);
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (empty($errors)) {
+        $query = "insert into notes (body, user_id) values(:body, :user_id)";
+        $db->query($query, [
+            'body' => filter_var($_POST['body'], FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'user_id' => 1
+        ]);
+    }
 }
 
 
