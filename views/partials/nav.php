292009893b145d8a27dc322391f95175dc0c69ba
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-20 15:29:16 +0530

    Reverted nav.php href to fix nested links

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -13,14 +13,14 @@
                             class="<?= isUrl('/laracastphp/') ? 'bg-gray-900' : null ?> text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                             aria-current="page">Home</a>
 
-                        <a href="about"
+                        <a href="/laracastphp/about"
                             class="<?= isUrl('/laracastphp/about') ? 'bg-gray-900' : null ?> text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About
                             Us</a>
 
-                        <a href="notes"
+                        <a href="/laracastphp/notes"
                             class="<?= isUrl('/laracastphp/notes') ? 'bg-gray-900' : null ?> text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Notes</a>
 
-                        <a href="contact"
+                        <a href="/laracastphp/contact"
                             class="<?= isUrl('/laracastphp/contact') ? 'bg-gray-900' : null ?> text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact
                             Us</a>
                     </div>
