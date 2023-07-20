838a4652dd65c26bfba6787e19fb86b47fcf281f
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-20 16:12:24 +0530

    required textarea

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -11,7 +11,7 @@ $db = new Database($config['database'], 'ajin', '123456');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $query = "insert into notes (body, user_id) values(:body, :user_id)";
 
-    $notes = $db->query($query, [
+    $db->query($query, [
         'body' => filter_var($_POST['body'], FILTER_SANITIZE_FULL_SPECIAL_CHARS),
         'user_id' => 1
     ]);
