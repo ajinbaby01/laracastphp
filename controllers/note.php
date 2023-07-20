41ec0a023666f5c53d1d416410a87b214f73522f
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-20 12:27:50 +0530

    403 page to prevent unauthorized access

--- controllers/note.php
+++ controllers/note.php
@@ -11,6 +11,16 @@ $db = new Database($config['database'], 'ajin', '123456');
 $query = "select * from notes where id = :id";
 
 
-$note = $db->query($query, [':id' => $_GET['id']])->fetch();
+$note = $db->query($query, [
+    ':id' => $_GET['id']
+    ])->fetch();
+
+if(! $note){
+    abort();
+}
+
+if($note['user_id'] === 1){
+    abort(403);
+}
 
 require "views/note.view.php";
