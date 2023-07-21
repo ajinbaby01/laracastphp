266b3fb605022ee633d03d6ba628167743cb81f0
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-21 11:45:06 +0530

    spl autoloader

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
 
-require 'functions.php';
-require 'Response.php';
-require 'Database.php';
-require 'router.php';
+const BASE_PATH = __DIR__.'/../';
+
+require BASE_PATH.'functions.php';
+
+spl_autoload_register(function ($class) {
+    require base_path($class . '.php');
+});
+// Loads classes automatically when required
+
+require base_path('router.php');
 
 ?>
