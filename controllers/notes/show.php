a1426094995e0d719c83e257a4e70b23ecc70975
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-21 14:53:56 +0530

    Adding option to delete note with authorization

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,22 +1,42 @@
 <?php
 use Core\Database;
 
-$currentUserId = 1;
+$currentUserId = 25;
 
 $config = require base_path('config.php');
 
 $db = new Database($config['database'], 'ajin', '123456');
 
-$query = "select * from notes where id = :id";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // form was submitted
 
+    //making sure note deleted is by the current user
+    $query = "select * from notes where id = :id";
 
-$note = $db->query($query, [
-    ':id' => $_GET['id']
-])->findOrFail();
+    $note = $db->query($query, [
+        'id' => $_POST['id']
+    ])->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+    authorize($note['user_id'] === $currentUserId);
 
-view('notes/show.view.php', [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    $query = "delete from notes where id = :id";
+    $db->query($query, [
+        'id' => $_POST['id']
+    ]);
+
+    header('location: /notes');
+    exit;
+} else {
+    $query = "select * from notes where id = :id";
+
+    $note = $db->query($query, [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view('notes/show.view.php', [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -4,12 +4,18 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p class="mb-6" >
+        <p class="mb-6">
             <a class="text-blue-500 underline" href="notes">Go back to all notes</a>
         </p>
         <p>
             <?= $note['body']; ?>
         </p>
+
+        <form class="mt-6" method="POST">
+            <input type="hidden" name="id" value="<?= $_GET['id'] ?>">
+            <button class="text-red-500 text-sm">Delete</button>
+        </form>
+
     </div>
 </main>
 
