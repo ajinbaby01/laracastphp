50963906d2154511273cfc3a8d7c0af3ca12305b
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-21 12:19:29 +0530

    Moved some files to Core

--- Core/router.php
+++ Core/router.php
@@ -1,17 +1,17 @@
 <?php
 
-$routes = require('routes.php');
+$routes = require base_path('routes.php');
 
 function abort($responseCode = Response::HTTP_NOT_FOUND)
 {
     http_response_code($responseCode);
-    require "views/{$responseCode}.php";
+    require base_path("views/{$responseCode}.php");
 }
 
 function routeToController($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     } else {
         abort();
     }
