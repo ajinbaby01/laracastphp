74afc5a349b8bce4045395e0883cebaebec6a702
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-25 11:09:50 +0530

    Added routes for login and sessions

--- routes.php
+++ routes.php
@@ -20,4 +20,8 @@ $router->get('/contact', 'controllers/contact.php');
 $router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
 
+$router->get('/login', 'controllers/session/create.php')->only('guest');
+$router->post('/session', 'controllers/session/store.php')->only('guest');
+
+
 ?>
