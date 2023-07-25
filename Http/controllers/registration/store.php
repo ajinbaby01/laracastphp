e71c2df144b105bda9e39d611327fbe030f4de54
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-25 17:14:02 +0530

    Refactoring

--- Http/controllers/registration/store.php
+++ Http/controllers/registration/store.php
@@ -7,22 +7,18 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 $registerForm = new RegisterForm();
-if (!$registerForm->validate($email, $password)) {
+if ($registerForm->validate($email, $password)) {
     // check if entered email and password is valid
+    $auth = new Authenticator();
+    if (!$auth->attempt($email, $password)) {
+        // check if user is already in the database
+        $registerForm->insert($email, $password);
+    }
+    redirect('/');
+} else {
     return view("registration/create.view.php", [
         'errors' => $registerForm->getErrors()
     ]);
-}
-
-$auth = new Authenticator();
-if ($auth->attempt($email, $password)) {
-    // check if user is already in the database
-    redirect('/');
-}
-else {
-    // user is not in database. insert user into database
-    $registerForm->insert($email, $password);
-    redirect('/');
 }
 
 ?>
