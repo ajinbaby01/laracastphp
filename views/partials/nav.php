6963d77c5f3f98044fdb26bfca0984f9503682c8
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-25 09:49:47 +0530

    Register and Login button to nav if not signed in

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -52,7 +52,14 @@
                                         alt="">
                                 </button>
                             <?php else: ?>
-                                <a class="text-white" href="/register">Register</a>
+                                <a href="/register"
+                                    class="<?= isUrl('/register') ? 'bg-gray-900' : null ?> text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                                    Register
+                                </a>
+                                <a href="/login"
+                                    class="<?= isUrl('/login') ? 'bg-gray-900' : null ?> text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                                    Login
+                                </a>
                             <?php endif; ?>
                         </div>
 
@@ -101,6 +108,4 @@
             </div>
         </div>
     </div>
-
-
 </nav>
