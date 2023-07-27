bd2c822b15e40099859b491da093f59e72ac6a06
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-27 10:51:29 +0530

    Using composer to autoload

--- public/index.php
+++ public/index.php
@@ -6,15 +6,17 @@ session_start();
 
 const BASE_PATH = __DIR__ . '/../';
 
-require BASE_PATH . 'Core/functions.php';
-
-spl_autoload_register(function ($class) {
-
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-    require base_path("{$class}.php");
-});
 // Loads classes automatically when required
+require BASE_PATH . 'vendor/autoload.php'; // Composer autoloader
+
+// spl_autoload_register(function ($class) {
+
+//     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+
+//     require base_path("{$class}.php");
+// });
+
+require BASE_PATH . 'Core/functions.php';
 
 require base_path('bootstrap.php');
 
